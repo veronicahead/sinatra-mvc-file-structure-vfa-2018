c219ef02433756e1d8e9a45d8daac6401400e91e
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-29 01:55:30 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,11 +1,12 @@
 class Dog
   
   attr_accessor :name, :breed, :age
-  
-  def initialize
+  DOGS = []
+  def initialize (name, breed, age)
     @name = name
     @breed = breed
     @age = age
+    DOGS << self
   end
   
   
